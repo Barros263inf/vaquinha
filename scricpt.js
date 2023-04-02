dfd6a82af691cfce67c268c2449c19198bc68ebe
Daniel Silva <barros263inf@gmail.com>
2023-04-01 23:47:44 -0300

    Update Html, Css, JS
    - fix problem with clipboard
    - solution: element needs to appear
    - add customization to scroll bar
    - fix outline when in focus
    - add text area

--- scricpt.js
+++ scricpt.js
@@ -1,11 +1,9 @@
-
+    
 function getPixKey(key) {
-
     const data = key;
     data.select();
     data.setSelectionRange(0, 9999);
-    document.execCommand("copy");
-
+    alert(`Chave Copiada: ${data.value}`);
+    //document.execCommand("copy");
     navigator.clipboard.writeText(data.value);
-    alert(`chave copiada: ${data.value}`);
 }
