523a799fc74f9e09f336810448914be46b84c4ce
Daniel Silva <barros263inf@gmail.com>
2023-04-01 23:50:25 -0300

    Update scricpt.js
    - add 'execCommand("copy")'

--- scricpt.js
+++ scricpt.js
@@ -1,9 +1,9 @@
-    
+
 function getPixKey(key) {
     const data = key;
     data.select();
     data.setSelectionRange(0, 9999);
     alert(`Chave Copiada: ${data.value}`);
-    //document.execCommand("copy");
+    document.execCommand("copy");
     navigator.clipboard.writeText(data.value);
 }
