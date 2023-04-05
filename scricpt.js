0dced82e20892c416b4d49b562c919dee1093aa0
Daniel Silva <barros263inf@gmail.com>
2023-04-05 02:13:46 -0300

    Update Html and JS
    Html:
    - Fix pix key blanks
    - Rename functions
    - Add forgotten 'alt' parameter
    Script:
    - Separate copy function and show alert copy.

--- scricpt.js
+++ scricpt.js
@@ -1,9 +1,26 @@
+/*
+        I decided to use the "key" variable as global to split the copy functionality and show the alert.
+*/
+let key = "";
 
-function getPixKey(key) {
-    const data = key;
-    data.select();
-    data.setSelectionRange(0, 9999);
-    alert(`Chave Copiada: ${data.value}`);
-    document.execCommand("copy");
-    navigator.clipboard.writeText(data.value);
+function copyPixKey(data) {
+    key = data;
+    key.select();
+    key.setSelectionRange(0, 9999);
+    //navigator.clipboard.writeText(key.value);
+    document.execCommand('copy');
+
+    /*  I'm having problem with this command in mobile and desktop
+                navigator.clipboard.writeText(key.value)
+        In most of my searches the bug is related to using the command "alert( )".
+        Even after using "setTimeOut( )", to isolate the "alert( )" command, the problem persisted.
+        The "document.execCommand( )" command, despite being deprecated, is working perfectly on mobile and desktop
+    */
+        setTimeout(showPopOut, 100);
+}
+
+function showPopOut() {
+    if (key.length != 0) {
+        alert(`Chave Copiada: ${key.value}`);
+    }
 }
